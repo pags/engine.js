6a59425900ebb4d7942ece80b8c4ed57c0139a18
Justin Pagano <justinmpagano@gmail.com>
2016-01-23 12:49:22 -0800

    more comments

--- example/Application.js
+++ example/Application.js
@@ -1,4 +1,10 @@
+/**
+ * Main application.
+ */
 (function() {
+    // Just defining that "items" should be stored using the CollectionStore,
+    // which allows the management of collections of data under a single key,
+    // and saves to local storage.  Without this call, keys will use the default store (see README).
     curvilinear.model.registerStoreForKey({
         key: 'items',
         store: 'collection'
@@ -6,6 +12,8 @@
 
     var Application = BaseController.extend({
 
+        // If using require.js, the `text` plugin would allow templates to be managed within
+        // their own files, rather than be defined inline as a literal string.
         template: '<div id="summary"></div><div id="input"></div><div id="list"></div>',
 
         _createChildren: function() {
